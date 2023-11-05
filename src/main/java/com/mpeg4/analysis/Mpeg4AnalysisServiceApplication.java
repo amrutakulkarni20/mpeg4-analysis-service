0ad3a609ea5269dd9304553d8c4f5babff13da57
Amruta Kulkarni <130090485+amrutakulkarni20@users.noreply.github.com>
2023-11-05 15:16:11 +0100

    initial commit

---
+++ src/main/java/com/mpeg4/analysis/Mpeg4AnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mpeg4.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mpeg4AnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mpeg4AnalysisServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpeg4/analysis/Mpeg4AnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpeg4.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mpeg4AnalysisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
